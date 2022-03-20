34124abcd05a2e6d5b27c2913364c384a00347a2
leo-paul-ciron <leopaulciron@gmail.com>
2022-03-20 12:44:20 +0100

    page html and sciprt correction

--- index.js
+++ index.js
@@ -2,22 +2,37 @@
 
 
 function verification(valeur) {
-    if ( (Number.isInteger(valeur))) {
+    console.log(valeur)
+    
+    let exp = new RegExp("^[0-9]+$","g");
+
+    if (exp.test(valeur)) {
         return valeur;
     }
-    else{
+    else
+    {
         return null;
     }
+
+    
 }
 
 function conversion(valeur) {
     
     let celsius = verification(valeur);
+    console.log(celsius)
+    let fahrenheit = null;
 
     if (celsius != null) {
         let fahrenheit = (valeur * (9/5) +32 );
+        console.log(fahrenheit)
     }
     
     return fahrenheit
 }
 
+function main() {
+    let valeur = document.getElementById("InputConv").value;
+    
+    conversion(valeur);
+}
